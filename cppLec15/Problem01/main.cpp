eb7e86aee08abb1e2ac4d2dd84f465d1a9e87ccb
RinRainbow <105558966+RinRainbow@users.noreply.github.com>
2022-06-05 00:59:15 +0800

    Update main.cpp

--- cppLec15/Problem01/main.cpp
+++ cppLec15/Problem01/main.cpp
@@ -8,18 +8,18 @@ int main() {
     set<string> list;
     ifstream input("name.txt");
     string tmp;
-    
-    while(!input.eof()) {
-        getline(input, tmp);
+
+    while (getline(input, tmp)) {
         list.insert(tmp);
     }
-    
+
+    input.close();
+
     decltype(list)::iterator it = list.begin();
-    it++;
-    while(it != list.end()) {
+    while (it != list.end()) {
         cout << *it << endl;
         it++;
     }
-    
+
     return 0;
 }
